0f267d718d9209014ab24a7c28668b8342c2083d
Diogo Tavares <diogo.s.tavares@gmail.com>
2016-06-27 17:24:46 +0100

    Mapa a funcar
    Falta agora que o Judson ligue isto a BD para conseguir ir buscar os
    pontos de interesse

--- teste/app/src/main/java/travelcamel/teste/Menu1.java
+++ teste/app/src/main/java/travelcamel/teste/Menu1.java
@@ -1,7 +1,9 @@
 package travelcamel.teste;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu1 extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Menu1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu1);
+    }
+
+    public void abrirMapa(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
